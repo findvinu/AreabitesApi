9a48e40ac24b690b5c0a31f7cb54212ebd3ba292
AreabitesRepo <gwenosezuah@gmail.com>
2020-05-18 03:31:14 +0100

    startup class

--- Areabites/Startup.cs
+++ Areabites/Startup.cs
@@ -1,20 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Areabites
 {
     public class Startup
     {
+        public IConfiguration config;
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public Startup(IConfiguration config, IHostingEnvironment env)
+        {
+            this.config = config;
+            this.config = new ConfigurationBuilder()
+          .SetBasePath(Directory.GetCurrentDirectory())
+           .AddJsonFile("config.json", optional: false, reloadOnChange: true)
+           .AddJsonFile($"config.{env.EnvironmentName}.json", optional: true)
+           .AddEnvironmentVariables()
+           .Build();
+
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var connectionString = config.GetConnectionString("AreabitesConnection");
+            services.AddDbContextPool<DataContext>(
+            cfg =>
+            {
+                cfg.UseMySql(config.GetConnectionString("AreabitesConnection")).EnableSensitiveDataLogging();
+            });
+            services.AddMvc().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_1);
+            services.AddMvcCore()
+            .AddDataAnnotations()
+            .AddApiExplorer();
+            services.AddApiVersioning(options =>
+            {
+                options.ReportApiVersions = true;
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.DefaultApiVersion = new ApiVersion(1, 0);
+            });
+            services.AddMvcCore().AddVersionedApiExplorer(options =>
+            {
+                options.GroupNameFormat = "'v'VVV";
+                options.SubstituteApiVersionInUrl = true;
+            });
+            services.AddDistributedRedisCache(option =>
+            {
+                option.Configuration = "127.0.0.1";
+                option.InstanceName = "master";
+            });
+            services.AddSwaggerGen(x =>
+            {
+                x.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Areabites"
+                });
+                x.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+            });
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -23,12 +77,24 @@ namespace Areabites
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseHttpsRedirection();
+                app.UseAuthentication();
+                app.UseMvc(routes =>
+                {
+                    routes.MapRoute(
+                        name: "default",
+                        template: "{controller=Home}/{action=Index}/{id?}");
+                });
+                app.UseMvcWithDefaultRoute();
+                app.UseDefaultFiles();
+                app.UseStaticFiles();
+                app.UseSwagger();
+                app.UseSwaggerUI(x =>
+                {
+                    x.SwaggerEndpoint("/swagger/v1/swagger.json", "Areabites");
+                });
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
         }
     }
-}
+    }
